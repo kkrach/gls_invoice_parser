27f45a4bb0b6ccabd67ff3a2b11f4faa9a010eb9
Karl Krach <github@kkrach.de>
2025-01-17 21:03:51 +0100

    Implemented parser and tests

--- parser.js
+++ parser.js
@@ -7,21 +7,105 @@
  */
 
 
-function parse_invoice_text(text) {
+function parse_invoice_text(invoice_text) {
 
-	var recipient = "Charly";
-	var iban = "DE01 2345 6789 0123 4567 89"
-	var amount = "12.77";
-	var intended_use = "MA7798";
+	var recipient = "";
+	var iban = ""
+	var amount = "";
+	var intended_use = "";
+
+	var unparsable = "";
+	var invoice_lines = invoice_text.split("\n");
+	for (var cnt=0; cnt < invoice_lines.length; cnt++) {
+		var line = invoice_lines[cnt];
+		if (line.trim().length > 0) {
+			if ((result = line.match(/Empfänger\s*:?(.*)/i)) != null) {
+				// use next line, if result[1] is empty
+				if (result[1].trim().length == 0) result[1] = invoice_lines[++cnt];
+				if (!recipient) {
+					recipient = result[1];
+				}
+			}
+			else if ((result = line.match(/Inhaber\s*:?(.*)/i)) != null) {
+				// use next line, if result[1] is empty
+				if (result[1].trim().length == 0) result[1] = invoice_lines[++cnt];
+				if (!recipient) {
+					recipient = result[1];
+				}
+			}
+			else if ((result = line.match(/IBAN\s*:?\s*(D?E?[0-9 ]*)/i)) != null) {
+				// use next line, if result[1] is empty
+				if (result[1].trim().length == 0) result[1] = invoice_lines[++cnt];
+				if (!iban) {
+					iban = result[1];
+				}
+			}
+			else if ((result = line.match(/.*BIC\s*:?\s*([A-Z0-9]*)/i)) != null) {
+				// use next line, if result[1] is empty
+				if (result[1].trim().length == 0) result[1] = invoice_lines[++cnt];
+				//if (!bic) {
+				//	bic = result[1].replace(/\(.*\)/, "");
+				//}
+			}
+			else if ((result = line.match(/Summe\s*:?(.*)/i)) != null) {
+				// use next line, if result[1] is empty
+				if (result[1].trim().length == 0) result[1] = invoice_lines[++cnt];
+				if (!amount) {
+					amount = result[1].replace(/EUR|€/, "");
+				}
+			}
+			else if ((result = line.match(/Betrag\s*:?(.*)/i)) != null) {
+				// use next line, if result[1] is empty
+				if (result[1].trim().length == 0) result[1] = invoice_lines[++cnt];
+				if (!amount) {
+					amount = result[1].replace(/EUR|€/, "");
+				}
+			}
+			else if ((result = line.match(/Total\s*:?(.*)/i)) != null) {
+				// use next line, if result[1] is empty
+				if (result[1].trim().length == 0) result[1] = invoice_lines[++cnt];
+				if (!amount) {
+					amount = result[1].replace(/EUR|€/, "");
+				}
+			}
+			else if ((result = line.match(/Gesamt\s*:?(.*)/i)) != null) {
+				// use next line, if result[1] is empty
+				if (result[1].trim().length == 0) result[1] = invoice_lines[++cnt];
+				if (!amount) {
+					amount = result[1].replace(/EUR|€/, "");
+				}
+			}
+			else if ((result = line.match(/Verwendungszweck\s*:?\s*(.*)/i)) != null) {
+				// use next line, if result[1] is empty
+				if (result[1].trim().length == 0) result[1] = invoice_lines[++cnt];
+				intended_use = result[1];
+			}
+			else if ((result = line.match(/Betreff\s*:?(.*)/i)) != null) {
+				// use next line, if result[1] is empty
+				if (result[1].trim().length == 0) result[1] = invoice_lines[++cnt];
+				intended_use = result[1];
+			}
+			else {
+				unparsable += line + "\n";
+			}
+		}
+	}
+
+	// if there is only one line, which could not be parsed and there is no recipient yet, use this line
+	if (recipient == "" && unparsable.trim().split("\n").length == 1) {
+		recispient = unparsable.trim();
+	}
 
 	return {
-		recipient: recipient,
-		iban: iban,
-		amount: amount,
-		intended_use: intended_use
+		recipient: recipient.trim(),
+		iban: iban.trim(),
+		amount: amount.trim(),
+		intended_use: intended_use.trim(),
+		unparsable: unparsable
 	};
 }
 
+// needed for node.js tests
 if (typeof module != 'undefined') {
 	module.exports = { parse_invoice_text };
 }
