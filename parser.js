9a1c38bd0002bb4a7a3d950defb5734d0dfced2f
Karl Krach <github@kkrach.de>
2025-01-17 21:27:02 +0100

    Fix all tests

--- parser.js
+++ parser.js
@@ -17,72 +17,72 @@ function parse_invoice_text(invoice_text) {
 	var unparsable = "";
 	var invoice_lines = invoice_text.split("\n");
 	for (var cnt=0; cnt < invoice_lines.length; cnt++) {
-		var line = invoice_lines[cnt];
-		if (line.trim().length > 0) {
+		var line = invoice_lines[cnt].trim();
+		if (line) {
 			if ((result = line.match(/Empfänger\s*[: ]+(.*)/i)) != null) {
-				// use next line, if result[1] is empty
-				if (result[1].trim().length == 0) result[1] = invoice_lines[++cnt];
+				// use next line, as long result[1] is empty
+				while (cnt < invoice_lines.length && result[1].trim().length == 0) result[1] = invoice_lines[++cnt];
 				if (!recipient) {
 					recipient = result[1];
 				}
 			}
 			else if ((result = line.match(/Inhaber\s*:?(.*)/i)) != null) {
-				// use next line, if result[1] is empty
-				if (result[1].trim().length == 0) result[1] = invoice_lines[++cnt];
+				// use next line, as long result[1] is empty
+				while (cnt < invoice_lines.length && result[1].trim().length == 0) result[1] = invoice_lines[++cnt];
 				if (!recipient) {
 					recipient = result[1];
 				}
 			}
 			else if ((result = line.match(/IBAN\s*:?\s*(D?E?[0-9 ]*)/i)) != null) {
-				// use next line, if result[1] is empty
-				if (result[1].trim().length == 0) result[1] = invoice_lines[++cnt];
+				// use next line, as long result[1] is empty
+				while (cnt < invoice_lines.length && result[1].trim().length == 0) result[1] = invoice_lines[++cnt];
 				if (!iban) {
 					iban = result[1];
 				}
 			}
-			else if ((result = line.match(/.*BIC\s*:?\s*([A-Z0-9]*)/i)) != null) {
-				// use next line, if result[1] is empty
-				if (result[1].trim().length == 0) result[1] = invoice_lines[++cnt];
-				//if (!bic) {
-				//	bic = result[1].replace(/\(.*\)/, "");
-				//}
-			}
+//			else if ((result = line.match(/.*BIC\s*:?\s*([A-Z0-9]*)/i)) != null) {
+//				// use next line, as long result[1] is empty
+//				while (cnt < invoice_lines.length && result[1].trim().length == 0) result[1] = invoice_lines[++cnt];
+//				if (!bic) {
+//					bic = result[1].replace(/\(.*\)/, "");
+//				}
+//			}
 			else if ((result = line.match(/Summe\s*:?(.*)/i)) != null) {
-				// use next line, if result[1] is empty
-				if (result[1].trim().length == 0) result[1] = invoice_lines[++cnt];
+				// use next line, as long result[1] is empty
+				while (cnt < invoice_lines.length && result[1].trim().length == 0) result[1] = invoice_lines[++cnt];
 				if (!amount) {
 					amount = result[1].replace(/EUR|€/, "");
 				}
 			}
 			else if ((result = line.match(/Betrag\s*:?(.*)/i)) != null) {
-				// use next line, if result[1] is empty
-				if (result[1].trim().length == 0) result[1] = invoice_lines[++cnt];
+				// use next line, as long result[1] is empty
+				while (cnt < invoice_lines.length && result[1].trim().length == 0) result[1] = invoice_lines[++cnt];
 				if (!amount) {
 					amount = result[1].replace(/EUR|€/, "");
 				}
 			}
 			else if ((result = line.match(/Total\s*:?(.*)/i)) != null) {
-				// use next line, if result[1] is empty
-				if (result[1].trim().length == 0) result[1] = invoice_lines[++cnt];
+				// use next line, as long result[1] is empty
+				while (cnt < invoice_lines.length && result[1].trim().length == 0) result[1] = invoice_lines[++cnt];
 				if (!amount) {
 					amount = result[1].replace(/EUR|€/, "");
 				}
 			}
 			else if ((result = line.match(/Gesamt\s*:?(.*)/i)) != null) {
-				// use next line, if result[1] is empty
-				if (result[1].trim().length == 0) result[1] = invoice_lines[++cnt];
+				// use next line, as long result[1] is empty
+				while (cnt < invoice_lines.length && result[1].trim().length == 0) result[1] = invoice_lines[++cnt];
 				if (!amount) {
 					amount = result[1].replace(/EUR|€/, "");
 				}
 			}
 			else if ((result = line.match(/Verwendungszweck\s*:?\s*(.*)/i)) != null) {
-				// use next line, if result[1] is empty
-				if (result[1].trim().length == 0) result[1] = invoice_lines[++cnt];
+				// use next line, as long result[1] is empty
+				while (cnt < invoice_lines.length && result[1].trim().length == 0) result[1] = invoice_lines[++cnt];
 				intended_use = result[1];
 			}
 			else if ((result = line.match(/Betreff\s*:?(.*)/i)) != null) {
-				// use next line, if result[1] is empty
-				if (result[1].trim().length == 0) result[1] = invoice_lines[++cnt];
+				// use next line, as long result[1] is empty
+				while (cnt < invoice_lines.length && result[1].trim().length == 0) result[1] = invoice_lines[++cnt];
 				intended_use = result[1];
 			}
 			else {
