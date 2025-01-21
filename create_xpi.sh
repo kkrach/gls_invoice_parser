42c14d590d433518fea64e8cdca264d7b662d508
Karl Krach <github@kkrach.de>
2025-01-21 21:31:15 +0100

    Include version in XPI filename

--- create_xpi.sh
+++ create_xpi.sh
@@ -3,8 +3,10 @@
 # Creates the XPI file for this plugin
 #
 
-zip -FS gls_invoice_parsing.xpi manifest.json icons/*.png main.js parser.js _locales/*/*
+XPI_NAME=gls_invoice_parsing_$(cat manifest.json | jq -r .version).xpi
+
+zip -FS $XPI_NAME manifest.json icons/*.png main.js parser.js _locales/*/*
 
 echo
-echo "Create gls_invoice_parsing.xpi"
+echo "Create $XPI_NAME"
 echo
