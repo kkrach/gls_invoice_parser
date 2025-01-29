34015d13aade20e81db840504c8b1672f2b801e7
Karl Krach <github@kkrach.de>
2025-01-29 21:01:37 +0100

    Initial check-in of the changelog

--- create_xpi.sh
+++ create_xpi.sh
@@ -5,7 +5,7 @@
 
 XPI_NAME=gls_invoice_parsing_$(cat manifest.json | jq -r .version).xpi
 
-zip -FS $XPI_NAME manifest.json icons/*.png main.js parser.js _locales/*/*
+zip -FS $XPI_NAME manifest.json CHANGELOG main.js parser.js icons/*.png _locales/*/*
 
 echo
 echo "Create $XPI_NAME"
